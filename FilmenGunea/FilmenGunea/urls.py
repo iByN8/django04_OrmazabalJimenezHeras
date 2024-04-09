a70d9e51af617889af4416d4485bf79ebcc60c8b
jonanderjimenez <jjimenez062@ikasle.ehu.eus>
2024-04-09 10:57:53 +0200

    registeretalogged

--- FilmenGunea/FilmenGunea/urls.py
+++ FilmenGunea/FilmenGunea/urls.py
@@ -8,23 +8,22 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from app import forms, views
 
-
 urlpatterns = [
     path('', views.home, name='home'),
-    path('contact/', views.contact, name='contact'),
-    path('about/', views.about, name='about'),
+    path('register/', views.register, name='register'),
     path('login/',
-         LoginView.as_view
-         (
+         LoginView.as_view(
              template_name='app/login.html',
              authentication_form=forms.BootstrapAuthenticationForm,
-             extra_context=
-             {
+             extra_context={
                  'title': 'Log in',
-                 'year' : datetime.now().year,
-             }
+                 'year': datetime.now().year,
+             },
+             success_url='/logged'
          ),
          name='login'),
+    path('logged/', views.logged, name='logged'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('admin/', admin.site.urls),
 ]
+
--- FilmenGunea/app/views.py
+++ FilmenGunea/app/views.py
@@ -1,10 +1,8 @@
-"""
-Definition of views.
-"""
-
 from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpRequest
+from django.contrib.auth.decorators import login_required  
+
 
 def home(request):
     """Renders the home page."""
@@ -13,33 +11,25 @@ def home(request):
         request,
         'app/index.html',
         {
-            'title':'Home Page',
-            'year':datetime.now().year,
+            'title': 'Home Page',
+            'year': datetime.now().year,
         }
     )
+@login_required
+def logged(request):
+    """Render the logged page."""
+    return render(request, 'app/logged.html')
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
-def contact(request):
-    """Renders the contact page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/contact.html',
-        {
-            'title':'Contact',
-            'message':'Your contact page.',
-            'year':datetime.now().year,
-        }
-    )
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'app/register.html', {'form': form})
 
-def about(request):
-    """Renders the about page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/about.html',
-        {
-            'title':'About',
-            'message':'Your application description page.',
-            'year':datetime.now().year,
-        }
-    )
+
