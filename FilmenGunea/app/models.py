0b711fc7d800f3bd8f7adbe27ee2b92d8a2b1da2
Ibai Heras <98816018+iByN8@users.noreply.github.com>
2024-04-09 13:10:13 +0200

    a

--- FilmenGunea/app/models.py
+++ FilmenGunea/app/models.py
@@ -1,7 +1,20 @@
-"""
-Definition of models.
-"""
-
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Filma(models.Model):
+    izenburua = models.CharField(max_length=60)
+    zuzendaria = models.CharField(max_length=60)
+    urtea = models.IntegerField()
+    generoa = models.CharField(max_length=2)
+    sinopsia = models.CharField(max_length=700)
+    bozkak = models.IntegerField()
+
+    def __str__(self):
+        return self.izenburua
+
+class Bozkatzailea(models.Model):
+    erabiltzailea = models.ForeignKey(User, on_delete=models.CASCADE)
+    gogoko_filmak = models.ManyToManyField(Filma, related_name='bozkatzaileak')
+
+    def __str__(self):
+        return str(self.erabiltzailea)
